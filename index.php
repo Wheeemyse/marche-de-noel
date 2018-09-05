54a96351c2dcfa8678cb0ce580ff3c84730b1ef4
wheeemyse <talla.m.seck@gmail.com>
2018-09-05 11:18:33 +0200

    push test

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Marché de Noël de Charleville-Mézières</title>
+    <link rel="stylesheet" href="css/style.css">
   </head>
 
-  
+  <header>
+    <?php include('includes/header.html'); ?>
+  </header>
   <body>
 
   </body>
