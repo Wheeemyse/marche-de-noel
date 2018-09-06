ae436a932fd70382c1f98cf018a7f810f75d49e9
wheeemyse <talla.m.seck@gmail.com>
2018-09-06 14:16:53 +0200

    commit conflit

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <?php include('includes/header.html'); ?>
   </header>
   <body>
-
+    <h1>Bienvenue dans le monde magique de Noël et des légendes Ardennaises</h1>
   </body>
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <script type="text/javascript" src="js/header.js">
