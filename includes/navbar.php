8e09e08b5aceea13b92497ce124ba4d56d9c05de
Ingrid Amprou <ingridamprou@outlook.fr>
2018-09-07 17:09:42 +0200

    Menu

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,16 +5,7 @@
        <li class="onglet"><a href="index.php">Accueil</a></li>
        <li class="onglet"><a href="programme.php">Programme</a></li>
        <li class="onglet"><a href="legendes.php">Légendes</a></li>
-       <li class="onglet" class="dropdown"><a href="javascript:void(0)"  class="dropbtn">Animations <span>▼</span></a></li>
-          <div class="dropdown-content">
-            <a href="spectacles.php">Spectacles</a>
-            <a href="concerts.php">Concerts</a>
-            <a href="charlevill-lecture.php">Charlevill'lecture</a>
-            <a href="ateliers-feeriques.php">Ateliers féériques</a>
-            <a href="maneges.php">Les manèges</a>
-            <a href="patinoire.php">La patinoire</a>
-            <a href="corrida.php">La Corrida de Noël</a>
-          </div>
+       <li class="onglet"><a href="animations.php">Animations</a></li>
        <li class="onglet"><a href="galeries.php">Galeries</a></li>
        <li class="onglet"><a href="infos-pratiques.php">Infos pratiques</a></li>
      </ul>
