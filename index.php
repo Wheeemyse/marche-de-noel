29768ead51b2f2fc3cdb7509aa26f24328b40797
wheeemyse <talla.m.seck@gmail.com>
2018-09-07 11:39:30 +0200

    Modif index

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Marché de Noël de Charleville-Mézières</title>
     <link href="https://fonts.googleapis.com/css?family=Dancing+Script:700|Montserrat:500" rel="stylesheet">
     <link rel="stylesheet" href="font-awesome.min.css">
@@ -20,9 +21,15 @@
   </header>
   <body>
     <h1 class="welcome">Bienvenue dans le monde magique <br> des légendes ardennaises et de Noël!</h1>
+    <div class="plan">
+      <img class="planmdn" src="img/plan-mdn.jpg" alt="Plan du marché de Noël">
+      <a class="dlplan" href="img/plan-mdn.jpg" download="plan-mdn" >Telecharger le plan du marché de Noël</a>
+    </div>
   </body>
+
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <script type="text/javascript" src="js/header.js"></script>
+  <script type="text/javascript" src="js/navbar.js"></script>
   <script src="http://code.jquery.com/jquery-1.11.3.min.js"> </script>
   <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
