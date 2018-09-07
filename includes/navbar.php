9274a86a6ad644fe9b3656449c30c2286099f4a0
Ingrid Amprou <ingridamprou@outlook.fr>
2018-09-07 11:52:05 +0200

    Menu

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,15 +1,22 @@
 <html>
-  <section>
+  <section id="navbar">
    <nav>
-     <div></div>
      <ul>
-       <li data-xcoord="0px" class="active"><a href="index.php">Accueil</a></li>
-       <li data-xcoord="140px"><a href="programme.php">Programme</a></li>
-       <li data-xcoord="280px"><a href="legendes.php">Légendes</a></li>
-       <li data-xcoord="420px"><a href="animations.php">Animations</a></li>
-       <li data-xcoord="560px"><a href="exposants.php">Exposants</a></li>
-       <li data-xcoord="700px"><a href="galeries.php">Galeries</a></li>
-       <li data-xcoord="840px"><a href="infos-pratiques.php">Infos pratiques</a></li>
+       <li class="onglet"><a href="index.php">Accueil</a></li>
+       <li class="onglet"><a href="programme.php">Programme</a></li>
+       <li class="onglet"><a href="legendes.php">Légendes</a></li>
+       <li class="onglet" class="button-dropdown"><a href="javascript:void(0)" class="dropdown-toggle">Animations <span>▼</span></a></li>
+          <ul class="dropdown-menu">
+            <li class="onglet"><a href="spectacles.php">Spectacles</a></li>
+            <li class="onglet"><a href="concerts.php">Concerts</a></li>
+            <li class="onglet"><a href="charlevill-lecture.php">Charlevill'lecture</a></li>
+            <li class="onglet"><a href="ateliers-feeriques.php">Ateliers féériques</a></li>
+            <li class="onglet"><a href="maneges.php">Les manèges</a></li>
+            <li class="onglet"><a href="patinoire.php">La patinoire</a></li>
+            <li class="onglet"><a href="corrida.php">La Corrida de Noël</a></li>
+          </ul>
+       <li class="onglet"><a href="galeries.php">Galeries</a></li>
+       <li class="onglet"><a href="infos-pratiques.php">Infos pratiques</a></li>
      </ul>
    </nav>
   </section>
