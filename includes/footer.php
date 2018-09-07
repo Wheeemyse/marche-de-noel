a8e7638bea5ce48cabeaf9823dffc22436cf4b22
wheeemyse <talla.m.seck@gmail.com>
2018-09-07 17:18:26 +0200

    Modification index

---
+++ includes/footer.php
@@ -0,0 +1,45 @@
+		<footer class="footer-distributed">
+			<div class="footer-left">
+				<h3>Marché de <span>Noël</span></h3>
+
+				<p class="footer-links">
+					<a href="index.php">Accueil</a>
+					·
+					<a href="#">Programme</a>
+					·
+					<a href="#">Legendes ardennaises</a>
+					·
+					<a href="#">Animations</a>
+					·
+					<a href="#">Exposant</a>
+					·
+					<a href="#">Galeries</a>
+          ·
+					<a href="#">Informations pratiques</a>
+				</p>
+				<p class="footer-company-name">Mairie de charleville &copy; 2018</p>
+
+				<div class="footer-icons">
+					<!-- <a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-google-plus-square"></i></a> -->
+
+					<div class="icons-fb ich"></div>
+					<div class="icons-ig ich"></div>
+					<div class="icons-tw ich"></div>
+					<div class="icons-yt ich"></div>
+				</div>
+			</div>
+
+			<a class="fa fa-arrow-circle-up retourhaut" href="#"></a>
+
+			<div class="footer-right">
+				<form action="#" method="post">
+					<p>Contactez-nous</p><br><br>
+					<input type="text" id="formfooter" name="email" placeholder="Email">
+					<textarea name="message" placeholder="Message"></textarea>
+					<button>Envoyer</button>
+				</form>
+			</div>
+		</footer>
--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Marché de Noël de Charleville-Mézières</title>
+    <!-- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"> -->
     <link href="https://fonts.googleapis.com/css?family=Dancing+Script:700|Montserrat:500" rel="stylesheet">
     <link rel="stylesheet" href="font-awesome.min.css">
     <link rel="stylesheet" href="animate.min.css">
@@ -23,15 +25,14 @@
     <h1 class="welcome">Bienvenue dans le monde magique <br> des légendes ardennaises et de Noël!</h1>
     <div class="plan">
       <img class="planmdn" src="img/plan-mdn.jpg" alt="Plan du marché de Noël">
-      <a class="dlplan" href="img/plan-mdn.jpg" download="plan-mdn" >Telecharger le plan du marché de Noël</a>
+      <a class="plandl" href="img/plan-mdn.jpg" download="plan-mdn" >Telecharger le plan du marché de Noël</a>
     </div>
   </body>
+  <?php include('includes/footer.php'); ?>
 
   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <script type="text/javascript" src="js/header.js"></script>
   <script type="text/javascript" src="js/navbar.js"></script>
   <script src="http://code.jquery.com/jquery-1.11.3.min.js"> </script>
   <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
-
 </html>
