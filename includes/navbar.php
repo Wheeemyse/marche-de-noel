66c7bd09d986d62071d0f86c957977040cc7b0d8
Ingrid Amprou <ingridamprou@outlook.fr>
2018-09-07 16:16:41 +0200

    Menu dropdown ?

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,21 +5,19 @@
        <li class="onglet"><a href="index.php">Accueil</a></li>
        <li class="onglet"><a href="programme.php">Programme</a></li>
        <li class="onglet"><a href="legendes.php">Légendes</a></li>
-       <li class="onglet" class="button-dropdown"><a href="javascript:void(0)" class="dropdown-toggle">Animations <span>▼</span></a></li>
-          <ul class="dropdown-menu">
-            <li class="onglet"><a href="spectacles.php">Spectacles</a></li>
-            <li class="onglet"><a href="concerts.php">Concerts</a></li>
-            <li class="onglet"><a href="charlevill-lecture.php">Charlevill'lecture</a></li>
-            <li class="onglet"><a href="ateliers-feeriques.php">Ateliers féériques</a></li>
-            <li class="onglet"><a href="maneges.php">Les manèges</a></li>
-            <li class="onglet"><a href="patinoire.php">La patinoire</a></li>
-            <li class="onglet"><a href="corrida.php">La Corrida de Noël</a></li>
-          </ul>
+       <li class="onglet" class="dropdown"><a href="javascript:void(0)"  class="dropbtn">Animations <span>▼</span></a></li>
+          <div class="dropdown-content">
+            <a href="spectacles.php">Spectacles</a>
+            <a href="concerts.php">Concerts</a>
+            <a href="charlevill-lecture.php">Charlevill'lecture</a>
+            <a href="ateliers-feeriques.php">Ateliers féériques</a>
+            <a href="maneges.php">Les manèges</a>
+            <a href="patinoire.php">La patinoire</a>
+            <a href="corrida.php">La Corrida de Noël</a>
+          </div>
        <li class="onglet"><a href="galeries.php">Galeries</a></li>
        <li class="onglet"><a href="infos-pratiques.php">Infos pratiques</a></li>
      </ul>
    </nav>
   </section>
-
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 </html>
